4c76a4b4cac362081d04a78af53c29bf00de5684
TtheBC01 <tac688@alumni.stanford.edu>
2023-08-22 18:38:33 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import snickerdoodle_logo from './snickerdoodle_logo.png';
 
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { useWeb3Modal, Web3Modal, Web3Button } from '@web3modal/react'
+import { Web3Modal, Web3Button } from '@web3modal/react'
 import { configureChains, createConfig, WagmiConfig, useAccount, useSignMessage } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+import { arbitrum, mainnet, polygon, avalanche, avalancheFuji } from 'wagmi/chains'
+
+import "reflect-metadata"
+import { SnickerdoodleWebIntegration } from '@snickerdoodlelabs/web-integration';
 
 import { useEthersSigner } from './ethers';
 import './App.css';
 
-
-const chains = [arbitrum, mainnet, polygon]
-const projectId = '7b43f10fd3404bb16a3c0947b0ff3436'
+// Wallet Connect Configuration
+const chains = [arbitrum, mainnet, polygon, avalanche, avalancheFuji]
+console.log(process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID)
+if (!process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID) {
+  throw new Error("You need to provide a WalletConnect Project ID env variable");
+}
+const projectId = process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID;
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiConfig = createConfig({
@@ -21,8 +28,19 @@ const wagmiConfig = createConfig({
 })
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
-function App() {
+if (!process.env.REACT_APP_INFURA_API_KEY) {
+  throw new Error("You need to provide currently provide an Infura API env variable");
+}
 
+// Snickerdoodle Configuration
+const webIntegrationConfig = {
+  primaryInfuraKey: process.env.REACT_APP_INFURA_API_KEY,
+  ankrApiKey: process.env.REACT_APP_ANKR_API_KEY,
+  covalentApiKey: process.env.REACT_APP_COVALENT_API_KEY,
+  iframeURL: 'https://iframe.snickerdoodle.com',
+}
+
+function App() {
   return (
     <>
       <WagmiConfig config={wagmiConfig}>
@@ -45,20 +63,21 @@ function AskToSign() {
     message: 'Hello Snickerdoodle!',
   })
   const { isConnected } = useAccount()
-  let message 
-  if (isConnected) {
-    message = "Sign Message";
-   } else {
-    message = "Connect Wallet To Sign Message";
-   }
+  const message = "Sign Message";
+  const ethersSigner = useEthersSigner();
+  //const webIntegration = new SnickerdoodleWebIntegration(webIntegrationConfig, ethersSigner);
 
-  return (
-    <div>
-      <button onClick={() => signMessage()}>{message}</button>
-      {isSuccess && <div>Signature: {data}</div>}
-      {isError && <div>Error signing message</div>}
-    </div>
-  ) 
+  if (isConnected) {
+    return (
+      <div>
+        <button onClick={() => signMessage()}>{message}</button>
+        {isSuccess && <div>Signature: {data?.slice(0, 12) + "..." + data?.slice(data.length - 13, data.length - 1)}</div>}
+        {isError && <div>Error signing message</div>}
+      </div>
+    )
+  } else {
+    return <></>
+  }
 }
 
 export default App;
