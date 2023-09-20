ae47fca2a4abb6672eca6293b35fb35dd0c132ef
SnickerChar <charlie@snickerdoodlelabs.io>
2023-09-20 16:34:00 -0700

    Added ESLint. Fixed code style issues to match the rest of our stuff. Moved dependencies to dev-dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,55 @@
-import React from 'react';
-import snickerdoodle_logo from './snickerdoodle_logo.png';
+import "reflect-metadata";
+import { SnickerdoodleWebIntegration } from "@snickerdoodlelabs/web-integration";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal, Web3Button } from "@web3modal/react";
+import React from "react";
+import {
+  configureChains,
+  createConfig,
+  WagmiConfig,
+  useAccount,
+  useSignMessage,
+} from "wagmi";
+import {
+  arbitrum,
+  mainnet,
+  polygon,
+  avalanche,
+  avalancheFuji,
+} from "wagmi/chains";
 
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal, Web3Button } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig, useAccount, useSignMessage } from 'wagmi'
-import { arbitrum, mainnet, polygon, avalanche, avalancheFuji } from 'wagmi/chains'
-
-import "reflect-metadata"
-import { SnickerdoodleWebIntegration } from '@snickerdoodlelabs/web-integration';
-
-import { useEthersSigner } from './ethers';
-import './App.css';
+import { useEthersSigner } from "./ethers";
+import snickerdoodle_logo from "./snickerdoodle_logo.png";
+import "./App.css";
 
 // ------------------------- Wallet Connect Configuration ---------------------------
-const chains = [arbitrum, mainnet, polygon, avalanche, avalancheFuji]
-console.log(process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID)
+const chains = [arbitrum, mainnet, polygon, avalanche, avalancheFuji];
+console.log(process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID);
 if (!process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID) {
-  throw new Error("You need to provide a WalletConnect Project ID env variable");
+  throw new Error(
+    "You need to provide a WalletConnect Project ID env variable",
+  );
 }
 const projectId = process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID;
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-})
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 // ----------------------------------------------------------------------------------
 
 // ------------------------- Snickerdoodle Configuration ----------------------------
 if (!process.env.REACT_APP_INFURA_API_KEY) {
-  throw new Error("You need to provide currently provide an Infura API env variable");
+  throw new Error(
+    "You need to provide currently provide an Infura API env variable",
+  );
 }
 
 const webIntegrationConfig = {
@@ -39,17 +57,21 @@ const webIntegrationConfig = {
   ankrApiKey: process.env.REACT_APP_ANKR_API_KEY!,
   covalentApiKey: process.env.REACT_APP_COVALENT_API_KEY!,
   poapApiKey: process.env.REACT_APP_POAP_API_KEY,
-}
+};
 // -------------------------------------------------------------------------------------
 
 function App() {
-
   return (
     <>
       <WagmiConfig config={wagmiConfig}>
         <div className="App">
           <header className="App-header">
-            <a className='App-link' href="https://github.com/SnickerdoodleLabs/Testbed-React/tree/main" target='_blank' rel="noopener noreferrer">
+            <a
+              className="App-link"
+              href="https://github.com/SnickerdoodleLabs/Testbed-React/tree/main"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Snickerdoodle Labs React Testbed
             </a>
             <img src={snickerdoodle_logo} className="App-logo" alt="logo" />
@@ -65,34 +87,40 @@ function App() {
 }
 
 function AskToSign() {
-  const ethersSigner = useEthersSigner()
+  const ethersSigner = useEthersSigner();
   const { data, isError, isSuccess, signMessage } = useSignMessage({
-    message: 'Hello Snickerdoodle!',
-  })
-  const { isConnected } = useAccount()
+    message: "Hello Snickerdoodle!",
+  });
+  const { isConnected } = useAccount();
   const message = "Sign Message";
 
   React.useEffect(() => {
     if (isConnected && ethersSigner) {
-      const webIntegration = new SnickerdoodleWebIntegration(webIntegrationConfig, ethersSigner);
+      const webIntegration = new SnickerdoodleWebIntegration(
+        webIntegrationConfig,
+        ethersSigner,
+      );
       webIntegration.initialize();
     }
-  }, [isConnected, ethersSigner])
+  }, [isConnected, ethersSigner]);
 
   if (isConnected) {
     return (
       <div>
         <button onClick={() => signMessage()}>{message}</button>
-        {isSuccess &&
+        {isSuccess && (
           <div>
-            Signature: {data?.slice(0, 12) + "..." + data?.slice(data.length - 13, data.length - 1)}
+            Signature:{" "}
+            {data?.slice(0, 12) +
+              "..." +
+              data?.slice(data.length - 13, data.length - 1)}
           </div>
-        }
+        )}
         {isError && <div>Error signing message</div>}
       </div>
-    )
+    );
   } else {
-    return <></>
+    return <></>;
   }
 }
 
--- src/ethers.ts
+++ src/ethers.ts
@@ -1,24 +1,24 @@
-import * as React from 'react'
-import { type WalletClient, useWalletClient } from 'wagmi'
-import { providers } from 'ethers'
+import { providers } from "ethers";
+import * as React from "react";
+import { type WalletClient, useWalletClient } from "wagmi";
 
 export function walletClientToSigner(walletClient: WalletClient) {
-  const { account, chain, transport } = walletClient
+  const { account, chain, transport } = walletClient;
   const network = {
     chainId: chain.id,
     name: chain.name,
     ensAddress: chain.contracts?.ensRegistry?.address,
-  }
-  const provider = new providers.Web3Provider(transport, network)
-  const signer = provider.getSigner(account.address)
-  return signer
+  };
+  const provider = new providers.Web3Provider(transport, network);
+  const signer = provider.getSigner(account.address);
+  return signer;
 }
 
 /** Hook to convert a viem Wallet Client to an ethers.js Signer. */
 export function useEthersSigner() {
-  const { data: walletClient } = useWalletClient()
+  const { data: walletClient } = useWalletClient();
   return React.useMemo(
     () => (walletClient ? walletClientToSigner(walletClient) : null),
     [walletClient],
-  )
+  );
 }
